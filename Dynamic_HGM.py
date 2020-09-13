dbcbfc4a22765b43101a26cd4fa0e44dc94ff39e
Tingyi <tiwanyan@gmail.com>
2020-09-13 19:24:30 -0400

    time attention

--- Dynamic_HGM.py
+++ Dynamic_HGM.py
@@ -239,13 +239,15 @@ class dynamic_hgm():
         """
         #self.Dense_patient = tf.expand_dims(self.hidden_last,1)
         #self.hidden_last_comb = tf.concat([self.hidden_last,self.Dense_demo],2)
-        """
+
         self.hidden_att_e = tf.matmul(self.hidden_rep,self.weight_retain_w)
         self.hidden_att_e_softmax = tf.nn.softmax(self.hidden_att_e,1)
         self.hidden_att_e_broad = tf.broadcast_to(self.hidden_att_e_softmax,[tf.shape(self.input_x_vital)[0],self.time_sequence,1+self.positive_lab_size+self.negative_lab_size,self.latent_dim])
         self.hidden_mul = tf.multiply(self.hidden_att_e_broad,self.hidden_rep)
         self.hidden_final = tf.reduce_sum(self.hidden_mul,1)
-        """
+        self.hidden_last_comb = tf.concat([self.hidden_final,self.Dense_demo],2)
+        self.Dense_patient = self.hidden_last_comb
+
         """
         self.hidden_att_e = tf.math.sigmoid(tf.math.add(tf.matmul(self.hidden_last, self.weight_retain_variable_w),self.bias_retain_variable_b))
         #self.hidden_att_e_softmax = tf.nn.softmax(self.hidden_att_e, -1)
@@ -256,6 +258,7 @@ class dynamic_hgm():
         #self.Dense_patient = tf.expand_dims(self.hidden_rep,2)
         """
 
+        """
         self.hidden_att_e = tf.matmul(self.hidden_rep,self.weight_retain_w)
         self.hidden_att_e_softmax = tf.nn.softmax(self.hidden_att_e,1)
         self.hidden_att_e_broad = tf.broadcast_to(self.hidden_att_e_softmax,[tf.shape(self.input_x_vital)[0],self.time_sequence,1+self.positive_lab_size+self.negative_lab_size,self.latent_dim])
@@ -269,6 +272,7 @@ class dynamic_hgm():
 
         self.hidden_last_comb = tf.concat([self.hidden_final, self.Dense_demo], 2)
         self.Dense_patient = self.hidden_last_comb
+        """
 
         self.Dense_mortality_ = \
             tf.nn.relu(tf.math.add(tf.matmul(self.mortality,self.weight_mortality),self.bias_mortality))

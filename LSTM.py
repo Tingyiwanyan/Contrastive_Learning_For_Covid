71af7966d40f723ac394d04384146adf3e92a1d4
Tingyi <tiwanyan@gmail.com>
2020-10-08 01:46:28 -0400

    correct

--- LSTM.py
+++ LSTM.py
@@ -209,7 +209,7 @@ class LSTM_model():
         """
         Get interpretation matrix
         """
-        """
+
         self.braod_weight_variable = tf.broadcast_to(self.weight_projection_w, [tf.shape(self.input_x_vital)[0],
                                                                                 self.time_sequence,
                                                                                 self.latent_dim, self.latent_dim])
@@ -226,11 +226,11 @@ class LSTM_model():
                                                                                 self.latent_dim, self.latent_dim])
         self.project_weight_variable = tf.multiply(self.broad_hidden_att_e_variable, self.braod_weight_variable)
         self.project_weight_variable_final = tf.multiply(self.broad_hidden_att_e, self.project_weight_variable)
-        """
+
         """
         Get score important
         """
-        """
+
         self.time_feature_index = tf.constant([i for i in range(self.lab_size + self.item_size)])
         self.mortality_hidden_rep = tf.gather(self.weight_classification_w, self.time_feature_index, axis=0)
         #self.score_attention_ = tf.matmul(self.project_weight_variable_final,
@@ -239,7 +239,7 @@ class LSTM_model():
         self.score_attention_ = tf.matmul(self.project_weight_variable_final,self.mortality_hidden_rep)
         self.score_attention = tf.squeeze(self.score_attention_, [3])
         self.input_importance = tf.multiply(self.score_attention, self.input_x)
-        """
+
 
 
 

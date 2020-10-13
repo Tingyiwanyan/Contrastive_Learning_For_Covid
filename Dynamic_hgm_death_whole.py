bd77f221576071833b96460ea9f130aa4fc03e8c
Tingyi <tiwanyan@gmail.com>
2020-10-13 05:52:29 -0400

    correction

--- Dynamic_hgm_death_whole.py
+++ Dynamic_hgm_death_whole.py
@@ -29,7 +29,7 @@ class dynamic_hgm():
         self.latent_dim_cell_state = 100
         self.latent_dim_att = 100
         self.latent_dim_demo = 50
-        self.epoch = 10
+        self.epoch = 2
         self.item_size = len(list(kg.dic_vital.keys()))
         self.demo_size = len(list(kg.dic_race.keys()))
         self.lab_size = len(list(kg.dic_lab.keys()))
@@ -179,7 +179,7 @@ class dynamic_hgm():
         self.project_input = tf.math.add(tf.matmul(self.input_x, self.weight_projection_w), self.bias_projection_b)
         #self.project_input = tf.matmul(self.input_x, self.weight_projection_w)
         for i in range(self.time_sequence):
-            x_input_cur = tf.gather(self.project_input, i, axis=1)
+            x_input_cur = tf.gather(self.input_x, i, axis=1)
             if i == 0:
                 concat_cur = tf.concat([self.init_hiddenstate, x_input_cur], 2)
             else:
@@ -269,10 +269,10 @@ class dynamic_hgm():
         """
         Build dynamic HGM model
         """
-        #self.Dense_patient = tf.expand_dims(self.hidden_last,1)
-        #self.Dense_patient = tf.concat([self.hidden_last,self.Dense_demo],2)
+        self.Dense_patient = tf.expand_dims(self.hidden_last,1)
+        self.Dense_patient = tf.concat([self.hidden_last,self.Dense_demo],2)
 
-
+        """
         self.hidden_att_e = tf.matmul(self.hidden_rep,self.weight_retain_w)
         self.hidden_att_e_softmax = tf.nn.softmax(self.hidden_att_e,1)
         self.hidden_att_e_broad = tf.broadcast_to(self.hidden_att_e_softmax,[tf.shape(self.input_x_vital)[0],
@@ -288,7 +288,7 @@ class dynamic_hgm():
         self.hidden_final = tf.reduce_sum(self.hidden_mul_variable, 1)
         self.Dense_patient = tf.concat([self.hidden_final, self.Dense_demo], 2)
         #self.Dense_patient = tf.concat([self.hidden_mul_variable, self.Dense_demo], 2)
-
+        """
 
         #self.Dense_patient = self.hidden_last_comb
         # self.Dense_patient = tf.expand_dims(self.hidden_rep,2)
@@ -1043,7 +1043,7 @@ class dynamic_hgm():
         self.f1_test = 2 * (self.precision_test * self.recall_test) / (self.precision_test + self.recall_test)
 
         threshold = -1.01
-        self.resolution = 0.05
+        self.resolution = 0.01
         tp_test = 0
         fp_test = 0
         self.tp_total = []

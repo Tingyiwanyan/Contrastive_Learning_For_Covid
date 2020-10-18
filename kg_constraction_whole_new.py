f8747df12508c9b13b4da3eb2a83366ee1f214a0
Tingyi <tiwanyan@gmail.com>
2020-10-18 01:10:38 -0400

    correct icu filter

--- kg_constraction_whole_new.py
+++ kg_constraction_whole_new.py
@@ -117,7 +117,34 @@ class Kg_construct_ehr():
                 self.in_admit_time_ = [np.int(j) for j in self.in_admit_time[1].split(':')[0:-1]]
                 in_admit_time_value = self.in_admit_time_[0] * 60.0 + self.in_admit_time_[1]
                 total_in_admit_time_value = in_admit_date_value + in_admit_time_value
-                self.dic_patient[mrn_single].setdefault('Admit_times', []).append(total_in_admit_time_value)
+                self.dic_patient[mrn_single].setdefault('Admit_time_values', []).append(total_in_admit_time_value)
+            if self.covid_ar[i, 29] == self.covid_ar[i, 29]:
+                self.dic_patient[mrn_single]['icu_label'] = 1
+                in_time_single = self.covid_ar[i, 29]
+                self.in_time = in_time_single.split(' ')
+                in_date = [np.int(i) for i in self.in_time[0].split('-')]
+                in_date_value = (in_date[0] * 365.0 + in_date[1] * 30 + in_date[2]) * 24 * 60
+                self.in_time_ = [np.int(i) for i in self.in_time[1].split(':')[0:-1]]
+                in_time_value = self.in_time_[0] * 60.0 + self.in_time_[1]
+                total_in_time_value = in_date_value + in_time_value
+                self.dic_patient[mrn_single]['in_icu_time'] = self.in_time
+                self.dic_patient[mrn_single]['in_date'] = in_date
+                self.dic_patient[mrn_single]['in_time'] = self.in_time_
+                self.dic_patient[mrn_single]['total_in_icu_time_value'] = total_in_time_value
+            else:
+                self.dic_patient[mrn_single]['icu_label'] = 0
+
+        for i in self.dic_patient.keys():
+            self.dic_patient[i]['Admit_time_values'] = np.sort(self.dic_patient[i]['Admit_time_values'])
+            if self.dic_patient[i]['icu_label'] == 1:
+                if len(self.dic_patient[i]['Admit_time_values'])>1:
+                    if self.dic_patient[i]['total_in_icu_time_value']>self.dic_patient[i]['Admit_time_values'][1]:
+                        self.dic_patient[i]['icu_label'] = 0
+                        self.dic_patient[i]['filter_first_icu_visit'] = 1
+
+
+
+
 
 
 

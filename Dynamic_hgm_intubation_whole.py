35ae9372cd140dcd61c581d4c0b56d56a9906d32
Tingyi <tiwanyan@gmail.com>
2020-10-21 04:24:28 -0400

    correction

--- Dynamic_hgm_intubation_whole.py
+++ Dynamic_hgm_intubation_whole.py
@@ -22,8 +22,8 @@ class dynamic_hgm():
         #self.test_data = self.data_process.test_patient
         self.train_data_whole = self.data_process.train_patient_whole
         self.test_data_whole = self.data_process.test_patient_whole
-        self.length_train = len(self.train_data)
-        self.length_test = len(self.test_data)
+        #self.length_train = len(self.train_data)
+        #self.length_test = len(self.test_data)
         self.batch_size = 16
         self.time_sequence = 4
         self.time_step_length = 6

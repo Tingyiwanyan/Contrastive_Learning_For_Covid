ca9c45c48407e85f31833fb03d975208993664c5
Tingyi <tiwanyan@gmail.com>
2020-10-22 03:33:56 -0400

    correction

---
+++ Tsne_vis.py
@@ -0,0 +1,19 @@
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+k = np.load('7_data_icu_retain_cl_patient_data.npy')
+logit = np.load('test_logit_icu.npy')
+
+length = k.shape[0]
+x_embed =TSNE(n_components=2).fit_transform(k)
+
+
+for i in range(length):
+    if logit[i,0] == 1:
+        plt.plot(x_embed[i][0],x_embed[i][1],'.',color='red',markersize=6)
+    if logit[i,1] == 1:
+        plt.plot(x_embed[i][0],x_embed[i][1],'.',color='blue',markersize=10)
+
+plt.show()

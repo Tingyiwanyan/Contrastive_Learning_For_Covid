022b41b76b0cfa5449e56902c0d351152f6a354f
Tingyi <tiwanyan@gmail.com>
2020-10-22 00:51:24 -0400

    change to RNN CE intubation

--- kg_constraction_whole_new.py
+++ kg_constraction_whole_new.py
@@ -648,7 +648,7 @@ if __name__ == "__main__":
 
     random_pick_icu = random.sample(icu_data, 574)
     reduced_data_intubate = [i for i in kg.total_data_intubation if i not in random_pick_intubate]
-    kg.total_data_icu = reduced_data_intubate
+    #kg.total_data_icu = reduced_data_intubate
 
     process_data = kg_process_data(kg)
     process_data.separate_train_test()

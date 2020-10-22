374aa6309f56046ef983829e7c3bae6d65fc1d3b
Tingyi <tiwanyan@gmail.com>
2020-10-22 02:25:51 -0400

    correction

--- kg_constraction_whole_new.py
+++ kg_constraction_whole_new.py
@@ -646,9 +646,9 @@ if __name__ == "__main__":
     reduced_data_intubate = [i for i in kg.total_data_intubation if i not in random_pick_intubate]
     #kg.total_data_intubation = reduced_data_intubate
 
-    random_pick_icu = random.sample(icu_data, 850)
-    reduced_data_intubate = [i for i in kg.total_data_intubation if i not in random_pick_intubate]
-    kg.total_data_icu = reduced_data_intubate
+    random_pick_icu = random.sample(icu_data, 589)
+    reduced_data_icu = [i for i in kg.total_data_icu if i not in random_pick_icu]
+    kg.total_data_icu = reduced_data_icu
 
     process_data = kg_process_data(kg)
     process_data.separate_train_test()

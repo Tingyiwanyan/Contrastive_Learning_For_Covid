f47a03b473b262a1d5c3363be5652248953364d6
Tingyi <tiwanyan@gmail.com>
2020-10-23 22:34:38 -0400

    change to cl

--- Dynamic_hgm_icu_whole.py
+++ Dynamic_hgm_icu_whole.py
@@ -602,9 +602,9 @@ class dynamic_hgm():
         self.get_latent_rep_hetero()
         self.SGNN_loss()
         self.SGNN_loss_contrast()
-        self.train_step_neg = tf.compat.v1.train.AdamOptimizer(1e-3).minimize(self.negative_sum)
-        #self.train_step_neg = tf.compat.v1.train.AdamOptimizer(1e-3).minimize(
-            #0.8 * self.negative_sum + 0.2 * self.negative_sum_contrast)
+        #self.train_step_neg = tf.compat.v1.train.AdamOptimizer(1e-3).minimize(self.negative_sum)
+        self.train_step_neg = tf.compat.v1.train.AdamOptimizer(1e-3).minimize(
+            0.8 * self.negative_sum + 0.2 * self.negative_sum_contrast)
         # self.train_step_cross_entropy = tf.train.AdamOptimizer(1e-3).minimize(self.cross_entropy)
         self.sess = tf.InteractiveSession()
         tf.global_variables_initializer().run()

b9246d6399befff2bc8a808b98985f8f48b002d7
Tingyi <tiwanyan@gmail.com>
2020-10-24 19:44:20 -0400

    change to death 24h retain ce

--- Tsne_vis.py
+++ Tsne_vis.py
@@ -9,11 +9,31 @@ logit = np.load('test_logit_mortality.npy')
 length = k.shape[0]
 x_embed =TSNE(n_components=2).fit_transform(k)
 
+fig, axs = plt.subplots(2,2)
+"""
+for i in range(length):
+    if logit[i,0] == 1:
+        axs[0,0].plot(x_embed[i][0],x_embed[i][1],'.',color='red',markersize=6)
+    if logit[i,1] == 1:
+        axs[0,0].plot(x_embed[i][0],x_embed[i][1],'.',color='blue',markersize=10)
 
 for i in range(length):
     if logit[i,0] == 1:
-        plt.plot(x_embed[i][0],x_embed[i][1],'.',color='red',markersize=6)
+        axs[0,1].plot(x_embed[i][0],x_embed[i][1],'.',color='red',markersize=6)
     if logit[i,1] == 1:
-        plt.plot(x_embed[i][0],x_embed[i][1],'.',color='blue',markersize=10)
+        axs[0,1].plot(x_embed[i][0],x_embed[i][1],'.',color='blue',markersize=10)
+
+for i in range(length):
+    if logit[i,0] == 1:
+        axs[1,0].plot(x_embed[i][0],x_embed[i][1],'.',color='red',markersize=6)
+    if logit[i,1] == 1:
+        axs[1,0].plot(x_embed[i][0],x_embed[i][1],'.',color='blue',markersize=10)
+
+for i in range(length):
+    if logit[i,0] == 1:
+        axs[1,1].plot(x_embed[i][0],x_embed[i][1],'.',color='red',markersize=6)
+    if logit[i,1] == 1:
+        axs[1,1].plot(x_embed[i][0],x_embed[i][1],'.',color='blue',markersize=10)
 
 plt.show()
+"""

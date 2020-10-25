a5e5f242fcc7177d26eba8972b334d5a8bf4d7bc
Tingyi <tiwanyan@gmail.com>
2020-10-25 03:31:46 -0400

    correction

--- Tsne_vis.py
+++ Tsne_vis.py
@@ -54,6 +54,7 @@ plt.show()
 """
 Vis for intubation
 """
+"""
 intubate_ce_5 = np.load('5_data_intubate_ce_24h.npy')
 intubate_cl_5 = np.load('5_data_intubate_cl_24h.npy')
 intubate_ce_10 = np.load('10_data_intubate_ce_24h.npy')
@@ -73,6 +74,61 @@ length_5 = intubate_ce_5.shape[0]
 
 fig, axs = plt.subplots(2,2)
 fig.suptitle('Intubation Prediction')
+
+for i in range(length_10):
+    if logit_10[i,0] == 1:
+        axs[0,0].plot(x_embed_cl_10[i][0],x_embed_cl_10[i][1],'.',color='red',markersize=6)
+    if logit_10[i,1] == 1:
+        axs[0,0].plot(x_embed_cl_10[i][0],x_embed_cl_10[i][1],'.',color='blue',markersize=10)
+    axs[0, 0].set_title('A')
+
+for i in range(length_10):
+    if logit_10[i,0] == 1:
+        axs[0,1].plot(x_embed_ce_10[i][0],x_embed_ce_10[i][1],'.',color='red',markersize=6)
+    if logit_10[i,1] == 1:
+        axs[0,1].plot(x_embed_ce_10[i][0],x_embed_ce_10[i][1],'.',color='blue',markersize=10)
+    axs[0, 1].set_title('B')
+
+for i in range(length_5):
+    if logit_5[i,0] == 1:
+        axs[1,0].plot(x_embed_cl_5[i][0],x_embed_cl_5[i][1],'.',color='red',markersize=6)
+    if logit_5[i,1] == 1:
+        axs[1,0].plot(x_embed_cl_5[i][0],x_embed_cl_5[i][1],'.',color='blue',markersize=10)
+    axs[1, 0].set_title('C')
+
+for i in range(length_5):
+    if logit_5[i,0] == 1:
+        axs[1,1].plot(x_embed_ce_5[i][0],x_embed_ce_5[i][1],'.',color='red',markersize=6)
+    if logit_5[i,1] == 1:
+        axs[1,1].plot(x_embed_ce_5[i][0],x_embed_ce_5[i][1],'.',color='blue',markersize=10)
+    axs[1, 1].set_title('D')
+
+plt.show()
+"""
+
+"""
+Vis for ICU
+"""
+
+intubate_ce_5 = np.load('7_data_icu_ce_24h.npy')
+intubate_cl_5 = np.load('7_data_icu_cl_24h.npy')
+intubate_ce_10 = np.load('17_data_icu_ce_24h.npy')
+intubate_cl_10 = np.load('17_data_icu_cl_24h.npy')
+logit_5 = np.load('logit_7_icu.npy')
+logit_10 = np.load('logit_17_icu.npy')
+
+length_10 = intubate_ce_10.shape[0]
+length_5 = intubate_ce_5.shape[0]
+x_embed_ce_5 = TSNE(n_components=2).fit_transform(intubate_ce_5)
+x_embed_cl_5 = TSNE(n_components=2).fit_transform(intubate_cl_5)
+x_embed_ce_10 = TSNE(n_components=2).fit_transform(intubate_ce_10)
+x_embed_cl_10 = TSNE(n_components=2).fit_transform(intubate_cl_10)
+
+length_10 = intubate_ce_10.shape[0]
+length_5 = intubate_ce_5.shape[0]
+
+fig, axs = plt.subplots(2,2)
+fig.suptitle('Icu Prediction')
 
 for i in range(length_10):
     if logit_10[i,0] == 1:

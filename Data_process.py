9ba7d64649b7daaab6c8e25b0560f2e653b5ccc1
Tingyi <tiwanyan@gmail.com>
2020-12-04 04:24:18 -0500

    correction

--- Data_process.py
+++ Data_process.py
@@ -49,7 +49,7 @@ class kg_process_data():
         for j in range(5):
             for i in self.train_mortality[j*self.test_num:(j+1)*self.test_num]:
                 self.test_patient.append(i)
-            self.train_patient = [i for i in self.kg.total_data_mortality if i not in self.test_patient]
+            self.train_patient = [i for i in self.train_mortality if i not in self.test_patient]
             self.train_mortality_whole.append(self.train_patient)
             self.train_validate_mortality_whole.append(self.test_patient)
             self.test_patient = []

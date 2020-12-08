20128810e1590437928d1deae41887afdfaba11b
Tingyi <tiwanyan@gmail.com>
2020-12-08 15:14:56 -0500

    correction

--- kg_constraction_whole_new.py
+++ kg_constraction_whole_new.py
@@ -782,7 +782,7 @@ if __name__ == "__main__":
             if i in kg.total_data_icu:
                 icu_data.append(i)
 
-    random_pick_death = random.sample(death_data,1230)
+    random_pick_death = random.sample(death_data,1200)
     reduced_data = [i for i in kg.total_data_mortality if i not in random_pick_death]
     kg.total_data_mortality = reduced_data
 
@@ -868,6 +868,7 @@ if __name__ == "__main__":
     dhgm.sess.close()
 
     del dhgm
+
 
     print("now training 24h RETAIN with CE mortality")
     dhgm = dynamic_hgm(kg, process_data, 4)
